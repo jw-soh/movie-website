d7a35ee95d33be0f4645e568bae2005e3259bb33
JW Soh <kylie@Kylies-MacBook-Air.local>
2021-08-05 08:38:54 +0800

    Add caching for shell assets in service worker

--- sw.js
+++ sw.js
@@ -1,14 +1,30 @@
+const staticCacheName = 'site-static';
+const assets = [
+  '/',
+  '/index.html',
+  'js/app.js',
+  'js/movie-notes.js',
+  '/css/custom.css',
+  '/img/brand-logo.png',
+  'https://kit.fontawesome.com/9db8b166e2.js'
+]
+
 // install service worker
 self.addEventListener('install', evt => {
-  console.log('service worker has been installed');
+  evt.waitUntil(
+    caches.open(staticCacheName).then(cache => {
+      console.log('caching shell assets!');
+      cache.addAll(assets);
+    })
+  )
 })
 
 // activate event
 self.addEventListener('activate', evt => {
-  console.log('service worker has been activated');
+  //console.log('service worker has been activated');
 })
 
 // fetch event
 self.addEventListener('fetch', evt => {
-  console.log('fetch event', evt);
+  //console.log('fetch event', evt);
 })
